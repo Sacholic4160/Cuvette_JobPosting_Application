f669a7d38fbd90c85fa4d419824a177d489d44be
Sachin Parmar <sachinparmar4160@gmail.com>
2024-10-18 18:39:30 +0530

    the index file for db for connection

---
+++ Backend/src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+
+
+const connectDB = async () => {
+    try {
+
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+        )
+        console.log(`MongoDB Connected at connection: ${connectionInstance.connection.host}`)
+
+
+    } catch (error) {
+        console.log('mongodb connection failed:', error)
+        process.exit(1);
+    }
+}
+
+export default connectDB;
