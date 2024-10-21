2fa14864788b951398087b10c672ce0dd87c5a8a
Sachin Parmar <sachinparmar4160@gmail.com>
2024-10-21 10:55:26 +0530

    the middleware for verifying the jwt

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,9 +1,26 @@
 import jwt from 'jsonwebtoken'
+import { Company } from '../models/company.model.js';
 
-const verifyJwt = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+export const verifyJwt = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]; // Extract token from header
+    
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized, token not provided' });
     }
-}
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify token
+      const company = await Company.findById(decoded.id);
+  
+      if (!company) {
+        return res.status(404).json({ message: 'Company not found' });
+      }
+  
+      req.company = company; // Attach company to request
+      next(); // Continue to the next middleware
+    } catch (error) {
+      res.status(401).json({ message: 'Unauthorized, token invalid or expired' });
+    }
+  };
+
+  
