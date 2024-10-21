103bda80a07e410cfde19e3382549129a562e25c
Sachin Parmar <sachinparmar4160@gmail.com>
2024-10-21 18:35:11 +0530

    the jwt verification method updated because it not fetching token

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,26 +1,33 @@
 import jwt from 'jsonwebtoken'
 import { Company } from '../models/company.model.js';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(process.cwd(), './.env') });
 
 export const verifyJwt = async (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1]; // Extract token from header
-    
-    if (!token) {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization").split(" ")[1];
+
+     if (!token) {
       return res.status(401).json({ message: 'Unauthorized, token not provided' });
     }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify token
-      const company = await Company.findById(decoded.id);
-  
-      if (!company) {
-        return res.status(404).json({ message: 'Company not found' });
-      }
-  
-      req.company = company; // Attach company to request
-      next(); // Continue to the next middleware
-    } catch (error) {
-      res.status(401).json({ message: 'Unauthorized, token invalid or expired' });
-    }
-  };
+    //console.log(token, process.env.ACCESS_TOKEN_SECRET)
 
-  
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // Verify token
+    //console.log(decoded);
+    const company = await Company.findById(decoded.id);
+    //console.log(company);
+
+    if (!company) {
+      return res.status(404).json({ message: 'Company not found' });
+    }
+
+    req.company = company; // Attach company to request
+    next(); // Continue to the next middleware
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized, token invalid or expired' });
+  }
+};
+
