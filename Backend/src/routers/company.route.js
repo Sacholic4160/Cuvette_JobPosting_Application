a3e074265bb47309421c185e05f0062fa86abd81
Sachin Parmar <sachinparmar4160@gmail.com>
2024-10-21 10:57:05 +0530

    the route for company

--- Backend/src/routers/company.route.js
+++ Backend/src/routers/company.route.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import { registerCompany, verifyEmail, verifyPhone } from '../controllers/company.controller.js';
 
 const router = express.Router();
 
 
 
-router.post('/regiser')
+router.post('/register', registerCompany)
+router.post('/verify-email', verifyEmail);
+router.post('/verify-phone', verifyPhone)
+
+
+export default router
